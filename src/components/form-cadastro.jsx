15a77ecb2eeeed0b8e6f61837992d0f6393dd21e
Alexandre Martins <martinso.alex@gmail.com>
2021-04-29 22:16:00 -0300

    stepper e useEffect hook

--- src/components/form-cadastro.jsx
+++ src/components/form-cadastro.jsx
@@ -1,20 +1,51 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { DadosPessoais } from "./dados-pessoais";
 import { DadosUsuario } from "./dados-usuario";
 import { DadosEntrega } from "./dados-entrega";
+import { Step, StepLabel, Stepper, Typography } from "@material-ui/core";
 
 export function FormCadastro({ submit, validarCpf }) {
 	const [etapa, setEtapa] = useState(0);
+	const [dados, setDados] = useState({});
+
+	useEffect(() => {
+		if (etapa === formularios.length - 1) submit(dados);
+	});
 
 	const formularios = [
-		<DadosUsuario submit={proximaEtapa} />,
-		<DadosPessoais submit={proximaEtapa} validarCpf={validarCpf} />,
-		<DadosEntrega submit={submit} />,
+		<DadosUsuario submit={coletaDados} />,
+		<DadosPessoais submit={coletaDados} validarCpf={validarCpf} />,
+		<DadosEntrega submit={coletaDados} />,
+		<Typography variant='h5'>Obrigado por se Cadastrar!</Typography>,
 	];
+
+	function coletaDados(novosDados) {
+		setDados({ ...dados, ...novosDados });
+		proximaEtapa();
+	}
 
 	function proximaEtapa() {
 		setEtapa(etapa + 1);
 	}
 
-	return <>{formularios[etapa]}</>;
+	return (
+		<>
+			<Stepper activeStep={etapa}>
+				<Step>
+					<StepLabel>Login</StepLabel>
+				</Step>
+				<Step>
+					<StepLabel>Pessoal</StepLabel>
+				</Step>
+				<Step>
+					<StepLabel>Entrega</StepLabel>
+				</Step>
+				<Step>
+					<StepLabel>Finalização</StepLabel>
+				</Step>
+			</Stepper>
+
+			{formularios[etapa]}
+		</>
+	);
 }
